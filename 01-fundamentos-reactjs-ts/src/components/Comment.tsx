805d604202c1f0216e107865965d44c0fc64b026
PeterBarboza <pedro.barboza.dev@gmail.com>
2022-10-20 15:52:04 -0300

    feat: Comment props type improved

--- 01-fundamentos-reactjs-ts/src/components/Comment.tsx
+++ 01-fundamentos-reactjs-ts/src/components/Comment.tsx
@@ -23,7 +23,10 @@ export function Comment({ content, onDeleteComment }: CommentProps) {
 
 	return (
 		<div className={styles.comment}>
-			<Avatar hasBorder={false} src="http://github.com/PeterBarboza.png" alt="" />
+			<Avatar 
+				hasBorder={false} 
+				src="http://github.com/PeterBarboza.png" 
+			/>
 
 			<div className={styles.commentBox}>
 				<div className={styles.commentContent}>
