50a2f9c709ba816856d89f93041f70e8af53a0e6
PeterBarboza <pedro.barboza.dev@gmail.com>
2022-10-20 19:11:33 -0300

    feat: Fonts added to the project; global default theme configured; Lint added to the project

--- 02-ignite-timer/src/App.tsx
+++ 02-ignite-timer/src/App.tsx
@@ -1,13 +1,19 @@
-import { Button } from "./components/Button";
+import { ThemeProvider } from 'styled-components'
+
+import { Button } from './components/Button'
+
+import { GlobalStyles } from './styles/global'
+import { defaultTheme } from './styles/themes/default'
 
 export function App() {
   return (
-    <>
-      <Button variant="primary"/>
-      <Button variant="secondary"/>
-      <Button variant="danger"/>
-      <Button variant="success"/>
-      <Button />
-    </>
+    <ThemeProvider theme={defaultTheme}>
+      <Button variant="primary" />
+      <Button variant="secondary" />
+      <Button variant="danger" />
+      <Button variant="success" />
+      <Button /> asdasdasdas
+      <GlobalStyles />
+    </ThemeProvider>
   )
 }
