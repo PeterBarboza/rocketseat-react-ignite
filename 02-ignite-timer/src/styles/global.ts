6c05e7bc74e569a55d2bec36c49898fed6c7d59d
PeterBarboza <pedro.barboza.dev@gmail.com>
2022-10-24 14:02:50 -0300

    feat: Countdown component styles created

--- 02-ignite-timer/src/styles/global.ts
+++ 02-ignite-timer/src/styles/global.ts
@@ -14,7 +14,9 @@ export const GlobalStyles = createGlobalStyle`
 
   body {
     background: ${(props) => props.theme['gray-900']};
-    color: ${(props) => props.theme['gray-300']}
+
+    color: ${(props) => props.theme['gray-300']};
+    -webkit-font-smoothing: antialiased
   }
 
   body, input, textarea, button {
